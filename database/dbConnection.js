d6cb806451d273955eb8a4c2c3fc7d6992ec88ab
Lucky <lk899202@gmail.com>
2024-06-04 21:49:40 +0530

    initial commit

---
+++ database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_STACK_BLOGGING_APP",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((error) => {
+      console.log(`error accrued white connecting to database ${error}`);
+    });
+};
+

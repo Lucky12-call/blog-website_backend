58dfc1d6855b425f9ccc603edeb94dd0458b76b9
Lucky <lk899202@gmail.com>
2024-06-05 09:31:30 +0530

    add online database for deployment

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_BLOGGING_APP",
+      dbName: "MERN_STACK_BLOGGING_WEBSITE",
     })
     .then(() => {
       console.log("Connected to database!");
